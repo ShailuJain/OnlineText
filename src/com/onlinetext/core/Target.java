f0a4e4942dc781d9065a274d752c1418856bf7bc
Nikhil Nagdev <nnagdev58@gmail.com>
2019-12-20 19:14:33 +0530

    Writing with post request on shrib.com

--- src/com/onlinetext/core/Target.java
+++ src/com/onlinetext/core/Target.java
@@ -1,10 +1,11 @@
 package com.onlinetext.core;
 
 import java.io.IOException;
+import java.net.ProtocolException;
 
 public interface Target {
     public String getText() throws IOException;
-    public boolean putText(String text);
+    public boolean putText(String text) throws IOException;
     public boolean appendText(String text);
     public String getDescription();
 }
